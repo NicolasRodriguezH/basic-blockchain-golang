24bdc96c450ba29fedd696796ec88be30cc61afa
Cristian Niño <cristiandavid113@gmail.com>
2022-06-18 21:09:23 -0500

    chore: define block

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+type Block struct {
+	Index int `json:index`
+	TimeStamp string `json:time_stamp`
+	Proof int `json:proof`
+	PreviousHash string `json:previous_hash`
+}
+
+type Blocks Block[]
+
 func main()  {
 	//
 }
